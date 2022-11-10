a01e9c802c3099d6f0046d0dbdef5bed635dcefc
Marcus <hymanmarcus95@gmail.com>
2022-11-10 14:36:07 -0500

    add Update/Delete UI

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
   path('drinks/', views.drinks_index, name='drinks_index'),
   path('drinks/<int:drink_id>/', views.drinks_detail, name='drinks_detail'),
   path('drinks/create/', views.DrinkCreate.as_view(), name='drink_create'),
+  path('drinks/<int:pk>/update/', views.DrinkUpdate.as_view(), name='drinks_update'),
+  path('drinks/<int:pk>/delete/', views.DrinkDelete.as_view(), name='drinks_delete'),
 ]
